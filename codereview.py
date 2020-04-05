5e4375bd83a6bd733c74dd7d12345d8abe46c467
Matthis <matthispralat@gmail.Com>
2020-04-05 14:27:54 -0400

    coderevieq

---
+++ codereview.py
@@ -0,0 +1,6 @@
+"""ceci est un test de code review """
+# plusieures questions se posent
+# - peut on avoir des correcteurs et des observateurs
+# - peut on commenter les lignes
+# - est ce pratique 
+# - y'a t'il un suivit
